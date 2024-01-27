7c7643381e9dc6f5c6bd09df86988a65f34b11bf
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-01-27 05:02:07 -0600

    Add logic to newGrid button

--- javaScript.js
+++ javaScript.js
@@ -1,22 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
     const gridContainer = document.getElementById("gridContainer");
 
-    for (let i = 0; i < 16; i++) {
-        const gridRow = document.createElement("div");
-        gridRow.classList.add("row");
-        gridContainer.appendChild(gridRow);
-        for (let j = 0; j < 16; j++) {
-            const gridPixel = document.createElement("div")
-            gridPixel.classList.add("pixel");
-            gridRow.appendChild(gridPixel);
-        }
-    }
+    newGrid(16);
+        
+    const newGridBtn = document.getElementById("newGridBtn");
+    newGridBtn.addEventListener("click", () => {
+        let size = prompt("How many pixels wide and high")
+        newGrid(size);
+    });
 
+    function newGrid(size) {
+        // Delete old grid
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.firstChild);
+        }
+
+        // Create new grid
+        for (let i = 0; i < size; i++) {
+            const gridRow = document.createElement("div");
+            gridRow.classList.add("row");
+            gridContainer.appendChild(gridRow);
+            for (let j = 0; j < size; j++) {
+                const gridPixel = document.createElement("div")
+                gridPixel.classList.add("pixel");
+                gridRow.appendChild(gridPixel);
+            }
+        }
+        
+        // Add eventListener to each pixel
         const gridPixels = document.querySelectorAll(".pixel");
         gridPixels.forEach(function(gridPixel) {
             gridPixel.addEventListener("mouseover", () => {
-                gridPixel.style.backgroundColor = "red";
+            gridPixel.style.backgroundColor = "red";
             });
         });
-        
+    };
 });
